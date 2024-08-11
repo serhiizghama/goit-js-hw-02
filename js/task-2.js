df7360884b6e6b7572784967fa6d90ef78d2eee6
serhii.zghama <serhii.zghama@edetek.com>
2024-08-11 15:20:15 +0200

    Fixed task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,17 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    const totalPrice = quantity * pricePerDroid;
-    if (customerCredits < totalPrice) {
-        return "Insufficient funds!";
+function formatMessage(message, maxLength) {
+    if (message.length <= maxLength) {
+        const expression = message.slice();
+        return expression;
+    } else {
+        const answer = `${message.slice(0, maxLength)}...`;
+        return answer;
     }
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
-console.log(makeTransaction(5, 3000, 23000));
-console.log(makeTransaction(3, 1000, 15000));
-console.log(makeTransaction(10, 5000, 8000));
-console.log(makeTransaction(8, 2000, 10000));
-console.log(makeTransaction(10, 500, 5000));
+
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
